1611f2328058b48b03710628fc5893f5e38cdc3e
YohannFin <yohann.fin13@gmail.com>
2020-07-31 17:13:15 +0200

    app/system: fix route and base_url app/css: add css file and route in header

---
+++ application/views/dashboard/welcome_message.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+	<body>
+
+		<div id="container">
+			<h1>MyEMS</h1>
+		</div>
+
+	</body>
+</html>
---
+++ application/views/template/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>MyEMS</title>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url()?>/assets/css/style.css" rel="stylesheet">
+</head>
+
+
+
+
