1125b75d318ea4f686f5d7c09685681a95a88e7f
YohannFin <yohann.fin13@gmail.com>
2020-08-01 17:06:11 +0200

    app: change view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -21,6 +21,6 @@ class Dashboard extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('template/header');
-		$this->load->view('dashboard/welcome_message');
+		$this->load->view('dashboard/login');
 	}
 }
