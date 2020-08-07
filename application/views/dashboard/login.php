9d19b3478bf29d139ee98012dd3a82b115386f9d
Marvin <44065639+Mrenoir@users.noreply.github.com>
2020-08-07 13:22:56 +0200

    Add css

--- application/views/dashboard/login.php
+++ application/views/dashboard/login.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 	<body>
+
 		<div class="preloader"></div>
 		<div id="container">
 			<h1>MyEMS</h1>
@@ -12,12 +13,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<br>
 		<b id="logout"><a href="logout">Logout</a></b>
     </body>
-<?php
-foreach ($onServiceName as $key){
-echo $key['grade'];
-echo " ". $key['Fullname'];
-echo  '<br>';
-}
-?>
+<div>
+	<h1>Personne en service </h1>
+	<table class="responstable">
+		<tr>
+		<th> Nom & Prénom </th>
+		<th> Grade </th>
+		</tr>
+		<tr>
+		<?php
+		foreach ($onServiceName as $key){
+			echo '<tr>';
+		echo "<td> ".$key['Fullname']."</td>";
+		echo "<td>".$key['grade']."</td>";
+			echo '</tr>';
+		}
+		?>
+		</tr>
+	</table>
 
+</div>
 </html>
