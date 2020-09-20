60d6d63d715e07b8415790099e95890feac1eee9
YohannFin <yohann.fin13@gmail.com>
2020-09-20 19:05:31 +0200

    app/refresh: remove auto refresh don't need anymore with ajax

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="120">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
