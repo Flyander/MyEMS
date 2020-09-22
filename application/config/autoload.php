ff235603ef09f5e936f7639a8e5976ee7f46392f
YohannFin <yohann.fin13@gmail.com>
2020-09-23 00:26:27 +0200

    app: autoload model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,6 +132,6 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('loginDatabase', 'services');
 
 $autoload['helper'] = array('url');
