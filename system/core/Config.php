fa39288ecafe5657fcf28829fe7ccd73f1225d10
YohannFin <yohann.fin13@gmail.com>
2020-09-23 00:45:12 +0200

    app/fix: fix base url

--- system/core/Config.php
+++ system/core/Config.php
@@ -98,7 +98,7 @@ class CI_Config
 				$base_url = (is_https() ? 'https' : 'http') . '://' . $server_addr
 					. substr($_SERVER['SCRIPT_NAME'], 0, strpos($_SERVER['SCRIPT_NAME'], basename($_SERVER['SCRIPT_FILENAME'])));
 			} else {
-				$base_url = 'http://localhost/';
+				$base_url = 'http://myems.me/';
 			}
 
 			$this->set_item('base_url', $base_url);
