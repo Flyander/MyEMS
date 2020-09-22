7736d6252be6820f9ec4787eebac8d9e9df2a68d
YohannFin <yohann.fin13@gmail.com>
2020-09-23 00:30:17 +0200

    blabla test

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,6 +132,6 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('loginDatabase', 'services');
+$autoload['model'] = array('LoginDatabase');
 
 $autoload['helper'] = array('url');
