e94cacd57eff1a2c1ba899caf312fe2c24b05a4a
Marvin <44065639+Mrenoir@users.noreply.github.com>
2020-11-16 16:48:03 +0100

    Manque que l'affichage propre

--- application/views/dashboard/listHour.php
+++ application/views/dashboard/listHour.php
@@ -1,3 +1,12 @@
 <?php foreach($hourWeek as $week) {
 	echo  $week['id'];
+	echo " ";
+	echo  $week['dateStart'];
+	echo " ";
+	echo  $week['dateEnd'];
+	echo " ";
+	echo  $week['county'];
+	echo " ";
+	echo  $week['id_user'];
+	echo "\n";
 }
