da896a5f83866dfa7a1377d622e7daf7bf536fd4
Marvin <44065639+Mrenoir@users.noreply.github.com>
2020-12-27 11:58:38 +0100

    Test

--- application/views/dashboard/rdv/rdv.php
+++ application/views/dashboard/rdv/rdv.php
@@ -64,6 +64,7 @@
 
 			<label for="dropdown" style="font-size: 100%">Attribution : </label>
 			<select id="idUser" name="heard_about_us_on" class="form-control" id="dropdown" required>
+				<option value="0"  > Non assigné </option>
 				<?php foreach ($userInfo as $user){ ?>
 					<option value="<?= $user['id']?> " >  [<?= $user['gradeName'] ?>] <?= $user['fullname'] ?> </option>
 				 <?php } ?>
