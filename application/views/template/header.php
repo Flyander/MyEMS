79cb1a947068a97a7c4c55d4e8f53f26a08daa75
YohannFin <yohann.fin13@gmail.com>
2021-01-20 11:50:22 +0100

    app/feat: add jquery-ui

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -28,8 +28,10 @@
     <link href="<?php echo base_url()?>assets/css/sidebar-themes.css" rel="stylesheet">
     <link href="<?php echo base_url()?>assets/css/magnific-popup.css" rel="stylesheet">
     <link href="<?php echo base_url()?>assets/fontawesome/css/all.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/jquery-ui/jquery-ui.min.css" rel="stylesheet">
 
     <script src="<?php echo base_url()?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url()?>assets/jquery-ui/jquery-ui.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/magnific.js"></script>
     <script src="<?php echo base_url()?>assets/js/mCustomScrollbar.concat.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/main.js"></script>
